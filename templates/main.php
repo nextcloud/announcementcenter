a904ead1bcf6ed19b63225ff519840e4e8985e71
Joas Schilling <nickvergessen@owncloud.com>
2015-09-15 10:18:59 +0200

    Add message for "empty content"

--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @var array $_
+ * @var \OCP\IL10N $l
  */
 script('announcementcenter', 'script');
 style('announcementcenter', 'style');
@@ -8,11 +9,15 @@ style('announcementcenter', 'style');
 
 <div id="app">
 	<div id="app-content">
-		<?php if ($_['is_admin']) {
-			print_unescaped($this->inc('part.add'));
-		} ?>
-
 		<div id="app-content-wrapper">
+			<?php if ($_['is_admin']) {
+				print_unescaped($this->inc('part.add'));
+			} ?>
+
+			<div id="emptycontent" class="<?php if ($_['is_admin']): ?>emptycontent-admin <?php endif; ?>hidden">
+				<h2><?php p($l->t('No Announcements')); ?></h2>
+				<p><?php p($l->t('There are currently no announcements…')); ?></p>
+			</div>
 		</div>
 	</div>
 </div>
