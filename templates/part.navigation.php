dcc6f4c08db5bfe2f80df8a919b9d9d51a109101
Joas Schilling <nickvergessen@owncloud.com>
2015-09-15 09:46:05 +0200

    Remove old template files

--- templates/part.navigation.php
+++
@@ -1,6 +0,0 @@
-<ul>
-	<li id="announcements-list"><a href="#"><?php p($l->t('Announcements')); ?></a></li>
-	<?php if ($_['is_admin']): ?>
-	<li id="new-announcement" class="new-announcement"><a href="#"><span><?php p($l->t('Add announcement')); ?></span></a></li>
-	<?php endif; ?>
-</ul>
