8f263bd5fdc9ee3ef64cf1101e161a342d795473
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-01-07 08:38:16 -0500

    [tx-robot] updated from transifex

---
+++ l10n/fi_FI.php
@@ -0,0 +1,19 @@
+<?php
+$TRANSLATIONS = array(
+"Announcements" => "Tiedotteet",
+"The subject is too long or empty" => "Aihe on joko liian pitkä tai tyhjä",
+"Delete" => "Poista",
+"Announcing…" => "Tiedotetaan…",
+"Announced!" => "Tiedotettu!",
+"Announcement does not exist anymore" => "Tiedotetta ei ole enää olemassa",
+"You announced %s" => "Tiedotit %s",
+"%s announced %s" => "%s tiedotti %s",
+"%1\$s announced “%2\$s”" => "%1\$s tiedotti “%2\$s”",
+"No Announcements" => "Ei tiedotteita",
+"There are currently no announcements…" => "Ei tiedotteita juuri nyt…",
+"Add announcement" => "Lisää tiedote",
+"Subject…" => "Aihe…",
+"Your announcement…" => "Tiedotteesi…",
+"Announce" => "Tiedota"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
