69227384a0d4fff2dbd7ac5e4685a78793ce0c8f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-01-20 02:31:10 -0500

    [tx-robot] updated from transifex

---
+++ l10n/sl.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "Objave",
+    "The subject is too long or empty" : "Zadeva je predolga oziroma je ni",
+    "Delete" : "Izbriši",
+    "Announcing…" : "Objavljanje ...",
+    "Announced!" : "Objavljeno",
+    "Announcement does not exist anymore" : "Objava ni več na voljo",
+    "You announced %s" : "Objavili ste %s",
+    "%s announced %s" : "Oseba %s je objavila %s",
+    "%1$s announced “%2$s”" : "Oseba %1$s je objavila “%2$s”",
+    "No Announcements" : "Ni objav",
+    "There are currently no announcements…" : "Trenutno ni objav ...",
+    "Add announcement" : "Dodaj objavo",
+    "Subject…" : "Zadeva ...",
+    "Your announcement…" : "Vaša objava",
+    "Announce" : "Objavi"
+},
+"nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);");
