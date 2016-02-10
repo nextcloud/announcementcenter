43f7b58277ed7d57a4a2c1f8adbdb5ccb8884800
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-02-10 02:31:10 -0500

    [tx-robot] updated from transifex

---
+++ l10n/tr.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "Duyurular",
+    "The subject is too long or empty" : "Konu çok uzun veya boş",
+    "Delete" : "Sil",
+    "Announcing…" : "Duyuruluyor...",
+    "Announced!" : "Duyuruldu!",
+    "Announcement does not exist anymore" : "Duyuru artık bulunmuyor",
+    "You announced %s" : "%s duyurdunuz",
+    "%s announced %s" : "%s duyuruldu %s",
+    "%1$s announced “%2$s”" : "%1$s duyuruldu \"%2$s\"",
+    "No Announcements" : "Duyuru yok",
+    "There are currently no announcements…" : "Şu anda duyuru bulunmamaktadır...",
+    "Add announcement" : "Duyuru ekle",
+    "Subject…" : "Konu...",
+    "Your announcement…" : "Senin duyuruların...",
+    "Announce" : "Duyur"
+},
+"nplurals=2; plural=(n > 1);");
