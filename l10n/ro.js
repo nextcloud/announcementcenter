a95216077018459fe258d1e6010bed06f08c8295
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-05-08 02:31:26 -0400

    [tx-robot] updated from transifex

---
+++ l10n/ro.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "Anunțuri",
+    "Delete" : "Șterge",
+    "Announcing…" : "Se anunță...",
+    "Announced!" : "Anunțat!",
+    "Announcement does not exist anymore" : "Anunțul nu mai există",
+    "You announced %s" : "Ai anunțat %s",
+    "%s announced %s" : "%s a anunțat %s",
+    "The subject is too long or empty" : "Subiectul este gol sau prea lung",
+    "%1$s announced “%2$s”" : "%1$s a anunțat “%2$s”",
+    "No Announcements" : "Niciun anunț",
+    "There are currently no announcements…" : "În acest moment nu există niciun anunț...",
+    "Add announcement" : " Adaugă anunț",
+    "Subject…" : "Subiect",
+    "Your announcement…" : "Anunțul tău...",
+    "Announce" : "Anunță"
+},
+"nplurals=3; plural=(n==1?0:(((n%100>19)||((n%100==0)&&(n!=0)))?2:1));");
