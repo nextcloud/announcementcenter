7204122a86775037001ea4212f25e786f5adfb3c
Joas Schilling <coding@schilljs.com>
2016-08-25 09:52:59 +0200

    Fix notifications for admins

--- lib/Notification/Notifier.php
+++ lib/Notification/Notifier.php
@@ -77,7 +77,7 @@ class Notifier implements INotifier {
 					$params[0] = $user->getDisplayName();
 				}
 
-				$announcement = $this->manager->getAnnouncement($notification->getObjectId(), false);
+				$announcement = $this->manager->getAnnouncement((int) $notification->getObjectId(), false, false, false);
 				$params[] = str_replace("\n", ' ', $announcement['subject']);
 
 				$notification->setParsedMessage($announcement['message'])
