af3e6b4f8599d2800d09c9856521ef4713d0b27a
Joas Schilling <coding@schilljs.com>
2016-08-25 09:25:09 +0200

    Add a dark version for the empty content page

--- templates/main.php
+++ templates/main.php
@@ -31,6 +31,7 @@ style('announcementcenter', [
 			} ?>
 
 			<div id="emptycontent" class="<?php if ($_['isAdmin']): ?>emptycontent-admin <?php endif; ?>hidden">
+				<div class="icon-announcement"></div>
 				<h2><?php p($l->t('No Announcements')); ?></h2>
 				<p><?php p($l->t('There are currently no announcements…')); ?></p>
 			</div>
