4b3fa9952932c38728189ef5abfb1dea6c783b74
Joas Schilling <coding@schilljs.com>
2018-03-05 11:54:32 +0100

    Make tests pass on Nextcloud 14 master
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -33,7 +33,7 @@ require_once __DIR__.'/../../../lib/base.php';
 // Fix for "Autoload path not allowed: .../announcementcenter/tests/testcase.php"
 \OC_App::loadApp('announcementcenter');
 
-if(!class_exists('PHPUnit_Framework_TestCase')) {
+if(!class_exists('\PHPUnit\Framework\TestCase')) {
 	require_once('PHPUnit/Autoload.php');
 }
 
