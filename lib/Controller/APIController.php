acbac5ab5eee4b53a9a6eca129b735e4d2cfc084
Joas Schilling <coding@schilljs.com>
2020-10-03 00:55:27 +0200

    Add OCS API
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ lib/Controller/APIController.php
@@ -0,0 +1,245 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2016, Joas Schilling <coding@schilljs.com>
+ *
+ * @author Joas Schilling <coding@schilljs.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\AnnouncementCenter\Controller;
+
+use OCA\AnnouncementCenter\Manager;
+use OCA\AnnouncementCenter\Model\Announcement;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\AppFramework\Http\Response;
+use OCP\AppFramework\OCSController;
+use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\BackgroundJob\IJobList;
+use OCP\IConfig;
+use OCP\IDBConnection;
+use OCP\IGroupManager;
+use OCP\IInitialStateService;
+use OCP\IL10N;
+use OCP\IRequest;
+use OCP\IUser;
+use OCP\IUserManager;
+use OCP\IUserSession;
+use OCA\AnnouncementCenter\BackgroundJob;
+
+class APIController extends OCSController {
+
+	/** @var IJobList */
+	protected $jobList;
+
+	/** @var IDBConnection */
+	protected $connection;
+
+	/** @var IGroupManager */
+	protected $groupManager;
+
+	/** @var IUserManager */
+	protected $userManager;
+
+	/** @var IL10N */
+	protected $l;
+
+	/** @var Manager */
+	protected $manager;
+
+	/** @var IConfig */
+	protected $config;
+
+	/** @var ITimeFactory */
+	protected $timeFactory;
+
+	/** @var IUserSession */
+	protected $userSession;
+
+	/** @var IInitialStateService */
+	protected $initialState;
+
+	public function __construct(string $appName,
+								IRequest $request,
+								IDBConnection $connection,
+								IGroupManager $groupManager,
+								IUserManager $userManager,
+								IJobList $jobList,
+								IL10N $l,
+								Manager $manager,
+								IConfig $config,
+								ITimeFactory $timeFactory,
+								IUserSession $userSession,
+								IInitialStateService $initialState) {
+		parent::__construct($appName, $request);
+
+		$this->connection = $connection;
+		$this->groupManager = $groupManager;
+		$this->userManager = $userManager;
+		$this->jobList = $jobList;
+		$this->l = $l;
+		$this->manager = $manager;
+		$this->config = $config;
+		$this->timeFactory = $timeFactory;
+		$this->userSession = $userSession;
+		$this->initialState = $initialState;
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 *
+	 * @param int $offset
+	 * @return DataResponse
+	 */
+	public function get(int $offset = 0): DataResponse {
+		$announcements = $this->manager->getAnnouncements($offset);
+		$data = array_map([$this, 'renderAnnouncement'], $announcements);
+		return new DataResponse($data);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 *
+	 * @param string $subject
+	 * @param string $message
+	 * @param string[] $groups,
+	 * @param bool $activities
+	 * @param bool $notifications
+	 * @param bool $comments
+	 * @return DataResponse
+	 */
+	public function add(string $subject, string $message, array $groups, bool $activities, bool $notifications, bool $comments): DataResponse {
+		if (!$this->manager->checkIsAdmin()) {
+			return new DataResponse(
+				['message' => 'Logged in user must be an admin'],
+				Http::STATUS_FORBIDDEN
+			);
+		}
+		$user = $this->userSession->getUser();
+		$userId = $user instanceof IUser ? $user->getUID() : '';
+
+		try {
+			$announcement = $this->manager->announce($subject, $message, $userId, $this->timeFactory->getTime(), $groups, $comments);
+		} catch (\InvalidArgumentException $e) {
+			return new DataResponse(
+				['error' => $this->l->t('The subject is too long or empty')],
+				Http::STATUS_BAD_REQUEST
+			);
+		}
+
+		if ($activities || $notifications) {
+			$this->jobList->add(BackgroundJob::class, [
+				'id' => $announcement->getId(),
+				'activities' => $activities,
+				'notifications' => $notifications,
+			]);
+		}
+
+		return new DataResponse($this->renderAnnouncement($announcement));
+	}
+
+	protected function renderAnnouncement(Announcement $announcement): array {
+		$displayName = $announcement->getUser();
+		$user = $this->userManager->get($announcement->getUser());
+		if ($user instanceof IUser) {
+			$displayName = $user->getDisplayName();
+		}
+
+		$result = [
+			'id'		=> $announcement->getId(),
+			'author_id'	=> $announcement->getUser(),
+			'author'	=> $displayName,
+			'time'		=> $announcement->getTime(),
+			'subject'	=> $announcement->getSubject(),
+			'message'	=> $announcement->getParsedMessage(),
+			'groups'	=> null,
+			'comments'	=> $announcement->getAllowComments() ? $this->manager->getNumberOfComments($announcement) : false,
+		];
+
+		if ($this->manager->checkIsAdmin()) {
+			$result['groups'] = $this->manager->getGroups($announcement);
+			$result['notifications'] = $this->manager->hasNotifications($announcement);
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @NoAdminRequired
+	 *
+	 * @param int $id
+	 * @return DataResponse
+	 */
+	public function delete(int $id): DataResponse {
+		if (!$this->manager->checkIsAdmin()) {
+			return new DataResponse(
+				['message' => 'Logged in user must be an admin'],
+				Http::STATUS_FORBIDDEN
+			);
+		}
+
+		$this->manager->delete($id);
+
+		return new DataResponse();
+	}
+
+	/**
+	 * @NoAdminRequired
+	 *
+	 * @param int $id
+	 * @return Response
+	 */
+	public function removeNotifications(int $id): DataResponse {
+		if (!$this->manager->checkIsAdmin()) {
+			return new DataResponse(
+				['message' => 'Logged in user must be an admin'],
+				Http::STATUS_FORBIDDEN
+			);
+		}
+
+		$this->manager->removeNotifications($id);
+
+		return new DataResponse();
+	}
+
+	/**
+	 * @NoAdminRequired
+	 *
+	 * @param string $pattern
+	 * @return DataResponse
+	 */
+	public function searchGroups(string $pattern): DataResponse {
+		if (!$this->manager->checkIsAdmin()) {
+			return new DataResponse(
+				['message' => 'Logged in user must be an admin'],
+				Http::STATUS_FORBIDDEN
+			);
+		}
+
+		$groups = $this->groupManager->search($pattern, 10);
+		$gids = [];
+		foreach ($groups as $group) {
+			$gids[] = $group->getGID();
+		}
+
+		return new DataResponse($gids);
+	}
+}
