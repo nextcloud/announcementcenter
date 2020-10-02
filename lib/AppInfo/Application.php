462cd22d1a493b422c5c3ab4a288427bd91572b7
Joas Schilling <coding@schilljs.com>
2020-10-03 00:55:49 +0200

    Always load the icon for dashboard and more
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -26,6 +26,7 @@ declare(strict_types=1);
 namespace OCA\AnnouncementCenter\AppInfo;
 
 use OCA\AnnouncementCenter\Dashboard\Widget;
+use OCA\AnnouncementCenter\Listener\BeforeTemplateRenderedListener;
 use OCA\AnnouncementCenter\Manager;
 use OCA\AnnouncementCenter\Model\AnnouncementDoesNotExistException;
 use OCA\AnnouncementCenter\Notification\Notifier;
@@ -33,6 +34,7 @@ use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
 use OCP\Comments\CommentsEntityEvent;
 use OCP\Notification\IManager;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -46,6 +48,7 @@ class Application extends App implements IBootstrap {
 
 	public function register(IRegistrationContext $context): void {
 		$context->registerDashboardWidget(Widget::class);
+		$context->registerEventListener(BeforeTemplateRenderedEvent::class, BeforeTemplateRenderedListener::class);
 	}
 
 	public function boot(IBootContext $context): void {
