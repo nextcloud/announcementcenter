fbce68750748289704c70094cee46475f02a5419
Joas Schilling <coding@schilljs.com>
2021-06-11 11:46:05 +0200

    Move and compile admin.js as well
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- webpack.js
+++ webpack.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
+webpackConfig.entry.admin = path.join(__dirname, 'src', 'admin')
 webpackConfig.entry.dashboard = path.join(__dirname, 'src', 'dashboard')
 
 module.exports = webpackConfig
