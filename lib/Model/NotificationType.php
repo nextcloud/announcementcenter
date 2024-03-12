721365c5aec0bace5b0ab0b3e22136015567ee99
Marvin Winkens <m.winkens@fz-juelich.de>
2024-03-12 13:53:55 +0100

    fix linting issues
    Signed-off-by: Marvin Winkens <m.winkens@fz-juelich.de>

--- lib/Model/NotificationType.php
+++ lib/Model/NotificationType.php
@@ -2,52 +2,44 @@
 
 namespace OCA\AnnouncementCenter\Model;
 
-class NotificationType
-{
-    protected $notificationTypes;
+class NotificationType {
+	protected $notificationTypes;
 
-    public function __construct()
-    {
-        $this->notificationTypes = [
-            'activities' => 0,
-            'notifications' => 1,
-            'email' => 2,
-        ];
-    }
+	public function __construct() {
+		$this->notificationTypes = [
+			'activities' => 0,
+			'notifications' => 1,
+			'email' => 2,
+		];
+	}
 
-    private function isTypeSet(int $value, string $notificationType) : bool
-    {
-        $offset = $this->notificationTypes[$notificationType];
-        return ($value & (1 << $offset)) > 0;
-    }
+	private function isTypeSet(int $value, string $notificationType) : bool {
+		$offset = $this->notificationTypes[$notificationType];
+		return ($value & (1 << $offset)) > 0;
+	}
 
-    private function getTypeMask(bool $booleanValue, string $notificationType) : int
-    {
-        $offset = $this->notificationTypes[$notificationType];
-        return ((int)$booleanValue) << $offset;
-    }
+	private function getTypeMask(bool $booleanValue, string $notificationType) : int {
+		$offset = $this->notificationTypes[$notificationType];
+		return ((int)$booleanValue) << $offset;
+	}
 
-    public function getActivities(int $value) : bool
-    {
-        return $this->isTypeSet($value, 'activities');
-    }
+	public function getActivities(int $value) : bool {
+		return $this->isTypeSet($value, 'activities');
+	}
 
-    public function getNotifications(int $value) : bool
-    {
-        return $this->isTypeSet($value, 'notifications');
-    }
+	public function getNotifications(int $value) : bool {
+		return $this->isTypeSet($value, 'notifications');
+	}
 
-    public function getEmail(int $value) : bool
-    {
-        return $this->isTypeSet($value, 'email');
-    }
+	public function getEmail(int $value) : bool {
+		return $this->isTypeSet($value, 'email');
+	}
 
-    public function setNotificationTypes(bool $activities, bool $notifications, bool $email) : int
-    {
-        $value = 0;
-        $value += $this->getTypeMask($activities, 'activities');
-        $value += $this->getTypeMask($notifications, 'notifications');
-        $value += $this->getTypeMask($email, 'email');
-        return $value;
-    }   
+	public function setNotificationTypes(bool $activities, bool $notifications, bool $email) : int {
+		$value = 0;
+		$value += $this->getTypeMask($activities, 'activities');
+		$value += $this->getTypeMask($notifications, 'notifications');
+		$value += $this->getTypeMask($email, 'email');
+		return $value;
+	}
 }
