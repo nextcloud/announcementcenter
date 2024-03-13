beb8095684786b96788802f913e99875c66901fa
Marvin Winkens <m.winkens@fz-juelich.de>
2024-03-13 13:13:58 +0100

    fix annoying typo preveting background job to work
    Signed-off-by: Marvin Winkens <m.winkens@fz-juelich.de>

--- lib/AnnouncementSchedulerJob.php
+++ lib/AnnouncementSchedulerJob.php
@@ -5,12 +5,14 @@ namespace OCA\AnnouncementCenter;
 use OCA\AnnouncementCenter\Service\AnnouncementSchedulerProcessor;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\BackgroundJob\TimedJob;
+use Psr\Log\LoggerInterface;
 
 class AnnouncementSchedulerJob extends TimedJob {
 
 	protected AnnouncementSchedulerProcessor $asp;
+	protected LoggerInterface $logger;
 
-	public function __construct(ITimeFactory $time, AnnouncementSchedulerProcessor $service) {
+	public function __construct(ITimeFactory $time, LoggerInterface $logger, AnnouncementSchedulerProcessor $service) {
 		parent::__construct($time);
 		$this->asp = $service;
 
