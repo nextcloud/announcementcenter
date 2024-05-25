59df5623508a8550a22b465085cacadacc29d318
Thomas Citharel <tcit@tcit.fr>
2024-05-25 20:02:32 +0200

    feat: use truncated message for notifications and activities
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- lib/Model/Announcement.php
+++ lib/Model/Announcement.php
@@ -72,8 +72,8 @@ class Announcement extends Entity {
 		return trim(str_replace("\n", ' ', $this->getSubject()));
 	}
 
-	public function getParsedMessage(): string {
-		return str_replace(['<', '>', "\n"], ['&lt;', '&gt;', '<br />'], $this->getMessage());
+	public function getTruncatedMessage(int $length = 100): string {
+		return substr($this->getPlainMessage(), 0, $length) . '…';
 	}
 
 	/**
